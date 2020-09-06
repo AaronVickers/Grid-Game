c092dfbdb14104ba2df8c201a338b822ec2fe5e4
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2020-09-06 16:45:02 +0100

    Change from 2 to 3 axis.

--- Program.cs
+++ Program.cs
@@ -32,23 +32,28 @@ namespace GridGame {
             Random rand = new Random();
             int randX = rand.Next(0, gridSize); // min inclusive, max exclusive
             int randY = rand.Next(0, gridSize);
+            int randZ = rand.Next(0, gridSize);
 
             int guesses = 0;
 
             while (true) {
                 Console.Write("Enter X co-ordinate: ");
                 string xInput = Console.ReadLine();
-                
+
                 Console.Write("Enter Y co-ordinate: ");
                 string yInput = Console.ReadLine();
+
+                Console.Write("Enter Z co-ordinate: ");
+                string zInput = Console.ReadLine();
 
                 try {
                     // Convert guess to integers
                     int xGuess = Convert.ToInt32(xInput);
                     int yGuess = Convert.ToInt32(yInput);
+                    int zGuess = Convert.ToInt32(zInput);
 
                     // Don't count guesses out of range
-                    if (xGuess < 0 || yGuess < 0 || xGuess >= gridSize || yGuess >= gridSize) {
+                    if (xGuess < 0 || yGuess < 0 || zGuess < 0 || xGuess >= gridSize || yGuess >= gridSize || zGuess >= gridSize) {
                         Console.WriteLine("Your guess was out of the grid's range.");
 
                         continue;
@@ -57,7 +62,7 @@ namespace GridGame {
                     guesses++; // Increase guess count AFTER validating input
 
                     // Exit loop if correct
-                    if (xGuess == randX && yGuess == randY) {
+                    if (xGuess == randX && yGuess == randY && zGuess == randZ) {
                         break;
                     }
 
@@ -83,13 +88,24 @@ namespace GridGame {
                         responseString += "too low";
                     }
 
+                    responseString += ", your Z is ";
+
+                    // Z result
+                    if (zGuess == randZ) {
+                        responseString += "correct";
+                    } else if (zGuess > randZ) {
+                        responseString += "too high";
+                    } else {
+                        responseString += "too low";
+                    }
+
                     Console.WriteLine(responseString+".");
                 } catch { // If guess is not of integer values
                     Console.WriteLine("Co-ordinates must be integers.");
                 }
             }
 
-            Console.WriteLine("You found the target at "+randX+", "+randY+"!");
+            Console.WriteLine("You found the target at "+randX+", "+randY+", "+randZ+"!");
             Console.WriteLine("It took you " + guesses + " guesses to find the target.");
         }
     }
